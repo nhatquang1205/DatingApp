e55525afcccfe66db0bf27c6e8cb3b11936a9be8
nhatquang1205 <trannhatquang2001@gmail.com>
2022-02-15 14:01:43 +0700

    Bai dau tien

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Database;
+using DatingApp.API.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async  Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+         [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ DatingApp.API/Database/Entities/User.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Database.Entities
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id {get;set;}
+        [Required]
+        [StringLength(32)]
+        public string Username { get; set; }     
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+    }
+}
