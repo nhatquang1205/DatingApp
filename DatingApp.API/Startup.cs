853c693e460338962633e45949756c4cb3f403f8
nhatquang1205 <trannhatquang2001@gmail.com>
2022-02-15 14:35:24 +0700

    Angular

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,6 +36,8 @@ namespace DatingApp.API
             });
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +55,8 @@ namespace DatingApp.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
